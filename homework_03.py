79a0a78950c0cdd124d14a2dfc2a1093ed43a2f7
casio250110 <casio250110@yandex.ru>
2022-12-19 00:04:22 +0300

    Доработали третью задачу

--- homework_03.py
+++ homework_03.py
@@ -1,12 +1,12 @@
 # Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, 
-# максимум использование библиотеки Random для и получения случайного int
+# максимум использование библиотеки Random для получения случайного int.
 
-import random
+from random import randint
 my_list=[]
 for i in range(10):
-    my_list.append(random.randint(0,100))
+    my_list.append(randint(0,100))
 print(my_list)
 for i in range(len(my_list)-1, 0, -1):
-    j = random.randint(0, i + 1) 
+    j = randint(0, i + 1) 
     my_list[i], my_list[j] = my_list[j], my_list[i]
 print(my_list)
